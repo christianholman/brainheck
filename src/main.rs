79bb79085b5b94afaa7bf0861164f02c1004741c
Christian Holman <c.holman@zohomail.eu>
2020-01-27 19:45:07 +0100

    Added ability to run program with args

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use bf::Interpreter;
 
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
     let mut interp = Interpreter::new();
-    interp.run(&std::fs::read_to_string("test").unwrap());
+    interp.run(&std::fs::read_to_string(args[1].clone()).expect("Error while reading file"));
 }
